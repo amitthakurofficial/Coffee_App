7315cdc2e8980e035b5c6b999b2ccc405a34dbce
amit <amit.thakur9851@gmail.com>
2019-02-26 21:32:36 +0530

    init commit

--- app/src/main/java/com/example/class_test/MainActivity.java
+++ app/src/main/java/com/example/class_test/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
 
     Button plus, minus;
     Button order,reset;
+    CheckBox ch1,ch2;
 
 
 
@@ -28,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
         minus=findViewById(R.id.button3);
         order=findViewById(R.id.button4);
         reset=findViewById(R.id.button7);
+        ch1=findViewById(R.id.checkBox);
+        ch2=findViewById(R.id.checkBox2);
 
         counter=findViewById(R.id.textView6);
 
@@ -55,14 +59,24 @@ public class MainActivity extends AppCompatActivity {
         order.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                int total=count*10;
                 if(count<1)
                 {
-                    Toast.makeText(MainActivity.this,"order please",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this,"order please!",Toast.LENGTH_SHORT).show();
 
                 }
                 else
                 {
-                    Toast.makeText(MainActivity.this,"your bill is"+String.valueOf(count*10),Toast.LENGTH_SHORT).show();
+                    if (ch1.isChecked())
+                    {
+                        total=total+ (count*5);
+                    }
+                    if (ch2.isChecked())
+                    {
+                        total=total+ (count*5);
+                    }
+
+                    Toast.makeText(MainActivity.this,"your bill is "+String.valueOf(total),Toast.LENGTH_SHORT).show();
                 }
 
 
@@ -72,15 +86,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                if (count > 1)
-                {
-                    count=0;
-                    counter.setText(String.valueOf(count));
-                }
+                counter.setText("0");
+                ch1.setChecked(false);
+                ch2.setChecked(false);
+                count=0;
 
             }
         });
-
     }
 
 }
